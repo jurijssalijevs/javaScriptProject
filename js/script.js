e212646690a6ee40fa5651f1d965ebbe93ff6715
Yura <jurijs.salijevs@gmail.com>
2021-01-20 00:51:57 +0200

    v5

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ if (personalMovieDB.count < 10) {
 } else if (personalMovieDB.count >= 30) {
   console.log('Вы киноман');
 } else {
-  console.log('Произошла ошибка');
+  console.log('Произошла ошибка!');
 }
 
 console.log(personalMovieDB);
