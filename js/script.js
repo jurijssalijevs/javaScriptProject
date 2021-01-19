756f5edc68f47fda2a7139909894fa2463b47f6d
Yura <jurijs.salijevs@gmail.com>
2021-01-20 00:51:11 +0200

    v4

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-const numberOfFilms = +prompt('Skolko filmov prosmotrel', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
-  genres: {},
+  genres: [],
   privat: false,
 };
 
-const a = prompt('Odin iz posle', '');
-b = prompt('rate', '');
-c = prompt('Odin iz posle', '');
-d = prompt('rate', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    console.log('done');
+    personalMovieDB.movies[a] = b;
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено слишком мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
-
-personalMovieDB.movies[a] = b;
